9a4092ff72b0f91405e0d55c4383bd74f5167773
Katie Lewis <klewi314@gmail.com>
2019-05-21 22:33:52 -0700

    Added .json

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,11 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Form from "./components/form/form.js";
 import NavBar from "./components/navbar/navbar.js";
+import FooterPage from "./components/footer/footer.js";
 
 function App() {
   return (
     <>
       <Form />
+      <FooterPage />
     </>
   );
 }
