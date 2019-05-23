34647f72e3a892e956d6765abfc225fb3e80908a
Katie Lewis <klewi314@gmail.com>
2019-05-22 20:46:07 -0700

    Initial upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/form";
+import NavBar from "./components/navbar";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
+
+export default App;
