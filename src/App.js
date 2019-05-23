b28b281a4af70fc629863b68c75947b6d5a2c9ea
Katie Lewis <klewi314@gmail.com>
2019-05-22 20:58:19 -0700

    Initial file upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Form from "./components/form";
-import NavBar from "./components/navbar";
-import Footer from "./components/footer";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Form />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
