c1f9308a2f6d067eb68f92b04fdbe78d760d4dad
Katie Lewis <klewi314@gmail.com>
2019-05-31 17:30:33 -0700

    Added bootstrap and card to about page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import About from "./components/About"
 import Portfolio from "./components/About"
 import Contact from "./components/About"
 import "./App.css"
-
+import { Button, Nav, NavItem, NavLink } from 'reactstrap';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
@@ -11,10 +11,28 @@ class App extends Component {
     return (
       <>
         <Router>
-          <div>
-            <Route path='./about' compnent={About} />
-            <Route path='./portfolio' compnent={Portfolio} />
-            <Route path='./contact' compnent={Contact} />
+        <div>
+          <nav>
+            <p> My awesome portfolio</p>
+            <Nav>
+              <NavItem>
+                <Link to='/'><NavLink>About</NavLink></Link>
+              </NavItem>
+              <NavItem>
+                <Link to='/portfolio'><NavLink>Portfolio</NavLink></Link>
+              </NavItem>
+              <NavItem>
+                <Link to='/contact'><NavLink>Contact</NavLink></Link>
+              </NavItem>
+            </Nav>
+
+            {/* <Link to='/'>  <Button color='success'>About</Button></Link> */}
+
+          </nav>
+          
+            <Route exact path='/' component={About} />
+            <Route path='/portfolio' component={Portfolio} />
+            <Route path='/contact' component={Contact} />
           </div>
         </Router>
       </>
